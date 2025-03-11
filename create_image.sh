d4981c85b4db24b81f1cbdbeeaf5c450571665ba
Bo Yu <yuzibode@126.com>
2025-03-11 10:19:43 +0800

    Use new parition for nvme rootfs
    Signed-off-by: Bo Yu <yuzibode@126.com>

--- create_image.sh
+++ create_image.sh
@@ -10,7 +10,7 @@ U_BOOT_IMG="sd-uboot.img"
 NVME_ROOTFS_IMG="${OUT_DIR}/${ROOTFS_IMG}"
 SD_UBOOT_IMG="${OUT_DIR}/${U_BOOT_IMG}"
 SD_DD_OPTS="bs=4k iflag=fullblock oflag=direct conv=fsync status=progress"
-PACKAGES_LIST="sudo openssh-server openntpd"
+PACKAGES_LIST="linux-image-riscv64 u-boot-menu u-boot-sifive sudo openssh-server openntpd"
 
 source $(pwd)/after_mkrootfs.sh
 
@@ -25,15 +25,20 @@ echo "Creating Blank nvme rootfs Image ${NVME_ROOTFS_IMG}"
 dd if=/dev/zero of="${NVME_ROOTFS_IMG}" bs=1M count=3072
 
 sgdisk -g --clear --set-alignment=1 \
-       --new=1:34:-1   --change-name=1:'rootfs'        --typecode=1:0x0700 --attributes=3:set:2  \
+       --new=1:34:+1M:    --change-name=1:'u-boot-spl'    --typecode=1:5b193300-fc78-40cd-8002-e86c45580b47 \
+       --new=2:2082:+4M:  --change-name=2:'opensbi-uboot' --typecode=2:2e54b353-1271-4842-806f-e436d6af6985 \
+       --new=3:16384:400M:   --change-name=3:'boot'      --typecode=3:0x0700  --attributes=3:set:2  \
+       --new=4:835584:-0   --change-name=4:'rootfs'       --typecode=4:0x8300 \
        ${NVME_ROOTFS_IMG} 
 
-#LOOPDEV=$(kpartx -av "${NVME_ROOTFS_IMG}")
+KPARTX_INFO=$(kpartx -av "${NVME_ROOTFS_IMG}")
+echo "KPARTX_INFO"
+LOOPDEV=$(kpartx -av "${NVME_ROOTFS_IMG}" | grep 'loop' | awk '{print $3}' | awk '{sub(/p[^p]*$/, ""); print}' | uniq)
 #echo "print ${LOOPDEV}"
-LOOPDEV=$(kpartx -av "${NVME_ROOTFS_IMG}"| awk '{print $3}')
+#LOOPDEV=$(losetup --partscan --find --show ${NVME_ROOTFS_IMG})
 echo "Nvme ${LOOPDEV}"
 
-if [ -z ${LOOPDEV} ]; then
+if [[ -z ${LOOPDEV} ]]; then
     echo "loopdev is empty"
     exit 1
 fi
@@ -42,17 +47,20 @@ fi
 #LOOPDEV="${LOOP}"
 echo "Partitioning loopback device ${LOOPDEV}"
 
-mkfs.ext4 -L rootfs -F "/dev/mapper/${LOOPDEV}"
+mkfs.ext4 -F -L boot "/dev/mapper/${LOOPDEV}p3"
+mkfs.ext4 -F -L rootfs  "/dev/mapper/${LOOPDEV}p4"
 
 # Copy Files, first the rootfs partition
 echo "Mounting  partitions ${LOOPDEV}"
-ROOTFS_POINT=/nvme_rootfs
+ROOTFS_POINT=/tmp/nvme_rootfs
 mkdir -p "${ROOTFS_POINT}"
 
-mount "/dev/mapper/${LOOPDEV}" "${ROOTFS_POINT}"
+mount "/dev/mapper/${LOOPDEV}p4" "${ROOTFS_POINT}"
 
 # rv64-port from mmdebstrap
 cp -a /builder/rv64-port/* "${ROOTFS_POINT}"
+
+mount "/dev/mapper/${LOOPDEV}p3" "${ROOTFS_POINT}/boot"
 
 mount -t proc /proc "${ROOTFS_POINT}/proc"
 mount -t sysfs /sys "${ROOTFS_POINT}/sys"
@@ -69,14 +77,16 @@ cp ${ROOTFS_POINT}/usr/lib/u-boot/sifive_unmatched/u-boot.itb ./u-boot.itb
 # need improve here also, we do not need another script
 after_mkrootfs
 
+
 # rv:rv
 chroot "${ROOTFS_POINT}" sh -c "useradd -m -s /bin/bash -G adm,cdrom,floppy,sudo,input,audio,dip,video,plugdev,netdev rv"
 chroot "${ROOTFS_POINT}" sh -c "echo 'rv:rv' | chpasswd"
 
 # root: unmatched
-ROOT_PASSWORD_HASH="$(echo 'unmatched' | openssl passwd -1 -stdin)"
-chroot "${ROOTFS_POINT}" sh -c "usermod --password '$ROOT_WORD_HASH' root"
+#ROOT_PASSWORD_HASH="$(echo 'unmatched' | openssl passwd -1 -stdin)"
+#chroot "${ROOTFS_POINT}" sh -c "usermod --password '$ROOT_WORD_HASH' root"
 #chroot "${ROOTFS_POINT}" sh -c "usermod --password "$(echo 'unmatched' | openssl passwd -1 -stdin)" root"
+chroot "${ROOTFS_POINT}" sh -c "export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true; apt clean"
 
 rm -v "${ROOTFS_POINT}"/etc/ssh/ssh_host_*
 chroot "${ROOTFS_POINT}" sh -c "apt clean" 
@@ -88,6 +98,7 @@ umount "${ROOTFS_POINT}/proc"
 umount "${ROOTFS_POINT}/sys"
 umount "${ROOTFS_POINT}/dev"
 
+umount "${ROOTFS_POINT}/boot"
 
 umount "${ROOTFS_POINT}" 
 
@@ -122,6 +133,6 @@ echo "Compressing the image: ${NVME_ROOTFS_IMG}"
 
 echo "Compressing the image: ${SD_ROOTFS_IMG}"
 
-(cd "${OUT_DIR}" && xz -T0 "${SD_ROOTFS_IMG}")
+(cd "${OUT_DIR}" && xz -T0 "${SD_UBOOT_IMG}")
 
 echo "Finishing the image..."
